385421a54c6b7edae8b31ad87e65117084a95117
nadavo11 <45533190+nadavo11@users.noreply.github.com>
2023-02-21 18:55:28 +0200

    Add files via upload

---
+++ xml2yolo.py
@@ -0,0 +1,70 @@
+"""
+This code converts your pascal XML formatted code to YOLO txt format
+make sure  that the datasets are structured as follows:
+root
+├──labels (folder)
+├  ├── 1.xml
+├  ├── 2.xml
+├  └── n.xml
+├──images (folder)
+├  ├── 1.jpg
+├  ├── 2.jpg
+├  └── n.jpg
+└──xml2yolo.py
+"""
+
+import xml.etree.ElementTree as ET
+import glob
+import os
+import json
+
+from xml_to_yolo_bbox import xml_to_yolo_bbox
+
+classes = [0]
+input_dir = "labels_xml/"
+output_dir = "labels/"
+image_dir = "images/"
+
+# create the labels folder (output directory)
+os.mkdir(output_dir)
+
+# identify all the xml files in the annotations folder (input directory)
+files = glob.glob(os.path.join(input_dir, '*.xml'))
+
+# loop through each
+for fil in files:
+    basename = os.path.basename(fil)
+    filename = os.path.splitext(basename)[0]
+    # check if the label contains the corresponding image file
+    if not os.path.exists(os.path.join(image_dir, f"{filename}.jpg")):
+        print(f"{filename} image does not exist!")
+        continue
+
+    result = []
+
+    # parse the content of the xml file
+    tree = ET.parse(fil)
+    root = tree.getroot()
+    width = int(root.find("size").find("width").text)
+    height = int(root.find("size").find("height").text)
+
+    for obj in root.findall('object'):
+        label = obj.find("name").text
+        # check for new classes and append to list
+        if label not in classes:
+            classes.append(label)
+        index = classes.index(label)
+        pil_bbox = [int(x.text) for x in obj.find("bndbox")]
+        yolo_bbox = xml_to_yolo_bbox(pil_bbox, width, height)
+        # convert data to string
+        bbox_string = " ".join([str(x) for x in yolo_bbox])
+        result.append(f"{index} {bbox_string}")
+
+    if result:
+        # generate a YOLO format text file for each xml file
+        with open(os.path.join(output_dir, f"{filename}.txt"), "w", encoding="utf-8") as f:
+            f.write("\n".join(result))
+
+# generate the classes file as reference
+with open('classes.txt', 'w', encoding='utf8') as f:
+    f.write(json.dumps(classes))
